b448c15535a6673351d21c1d76d2b5b0556fdbd1
qwerty_268 <lev-demchenko@bk.ru>
2022-10-04 15:40:35 +0300

    add new branch

---
+++ src/main/java/program/not/executable/importsubstitutionstatistics/ImportSubstitutionStatisticsApplication.java
@@ -0,0 +1,13 @@
+package program.not.executable.importsubstitutionstatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportSubstitutionStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportSubstitutionStatisticsApplication.class, args);
+	}
+
+}
